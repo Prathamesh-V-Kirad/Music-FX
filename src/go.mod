31ae90ff62c31250a484d5c54702978130e355e6
Prathamesh-V-Kirad <kiradvprathamesh2002@gmail.com>
2025-07-17 23:11:22 +0530

    Added lib to pick mp3 files

--- src/go.mod
+++ src/go.mod
@@ -4,7 +4,16 @@ go 1.23.2
 
 require (
 	github.com/buger/goterm v1.0.4
+	github.com/ncruces/zenity v0.10.14
 	github.com/pkg/term v1.1.0
 )
 
-require golang.org/x/sys v0.0.0-20210331175145-43e1dd70ce54 // indirect
+require (
+	github.com/TheTitanrain/w32 v0.0.0-20180517000239-4f5cfb03fabf // indirect
+	github.com/akavel/rsrc v0.10.2 // indirect
+	github.com/dchest/jsmin v0.0.0-20220218165748-59f39799265f // indirect
+	github.com/josephspurrier/goversioninfo v1.4.1 // indirect
+	github.com/randall77/makefat v0.0.0-20210315173500-7ddd0e42c844 // indirect
+	golang.org/x/image v0.20.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+)
---
+++ src/navigation.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"fmt"
+	"github.com/pkg/term"
+	"github.com/buger/goterm"
+)
+
+var up byte = 65
+var down byte = 66
+var esc byte = 27
+var enter byte = 13
+
+var keys = map[byte]bool{
+	up:    true,
+	down:  true,
+}
+
+type MenuItem struct {
+	Title string
+	ID string
+} 
+
+type Menu struct {
+	Prompt string
+	CursorPos int
+	MenuItems []*MenuItem
+}
+
+
+
+func (m *Menu) AddItem(title string, id string) *Menu{
+	item := &MenuItem{
+		Title: title,
+		ID:    id,
+	}
+	m.MenuItems = append(m.MenuItems, item)
+	return m
+}
+
+func getInput() byte{
+	inputBytes := make([]byte, 3)
+	t , _ := term.Open("/dev/tty")
+	err := term.RawMode(t)
+	if err != nil {
+		fmt.Println("Error opening terminal:", err)
+		
+	}
+	var inputLen int
+	inputLen , err = t.Read(inputBytes)
+	t.Restore()
+	t.Close()
+	if err != nil {		
+		fmt.Println("Error reading input:", err)
+	}
+	if inputLen == 3 {
+		if (keys[inputBytes[2]]){
+			return inputBytes[2]
+		}
+	}else{
+		return inputBytes[0]
+	}
+	return 0;	
+}
+
+
+func (m *Menu) renderMenuItems(redraw bool) {
+	if redraw {
+		fmt.Printf("\033[%dA", len(m.MenuItems)-1)
+	}
+
+	for index, menuItem := range m.MenuItems {
+		var newline = "\n"
+		if index == len(m.MenuItems)-1 {
+			newline = ""
+		}
+
+		menuItemText := menuItem.Title
+		cursor := "  "
+		if index == m.CursorPos {
+			cursor = goterm.Color("> ", goterm.YELLOW)
+			menuItemText = goterm.Color(menuItemText, goterm.YELLOW)
+		}
+
+		fmt.Printf("\r%s %s%s", cursor, menuItemText, newline)
+	}
+}
+
+func (m *Menu) Display() string {
+	defer func() {
+		fmt.Printf("\033[?25h")
+	}()
+
+	fmt.Printf("%s\n", goterm.Color(goterm.Bold(m.Prompt)+":", goterm.CYAN))
+
+	m.renderMenuItems(false)
+
+	fmt.Printf("\033[?25l")
+
+	for {
+		keyCode := getInput()
+		if keyCode == esc {
+			return ""
+		} else if keyCode == enter {
+			menuItem := m.MenuItems[m.CursorPos]
+			fmt.Println("\r")
+			return menuItem.ID
+		} else if keyCode == up {
+			m.CursorPos = (m.CursorPos + len(m.MenuItems) - 1) % len(m.MenuItems)
+			m.renderMenuItems(true)
+		} else if keyCode == down  {
+			m.CursorPos = (m.CursorPos + 1) % len(m.MenuItems)
+			m.renderMenuItems(true)
+			
+			
+		}
+	}
+}
+
+
